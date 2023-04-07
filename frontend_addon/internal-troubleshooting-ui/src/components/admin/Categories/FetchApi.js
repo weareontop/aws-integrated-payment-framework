419c223dae62611b687f0299a380cb9fd95b94f2
bitcooker <dreamhighpassionhigh@gmail.com>
2023-04-07 07:53:40 -0400

    chore : add editCategory func

--- frontend_addon/internal-troubleshooting-ui/src/components/admin/Categories/FetchApi.js
+++ frontend_addon/internal-troubleshooting-ui/src/components/admin/Categories/FetchApi.js
@@ -14,34 +14,48 @@ const Headers = () => {
 };
 
 export const getAllCategory = async () => {
-    try {
-      let res = await axios.get(`${apiURL}/api/category/all-category`, Headers());
-      return res.data;
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
-  export const createCategory = async ({
-    cName,
-    cImage,
-    cDescription,
-    cStatus,
-  }) => {
-    let formData = new FormData();
-    formData.append("cImage", cImage);
-    formData.append("cName", cName);
-    formData.append("cDescription", cDescription);
-    formData.append("cStatus", cStatus);
-  
-    try {
-      let res = await axios.post(
-        `${apiURL}/api/category/add-category`,
-        formData,
-        Headers()
-      );
-      return res.data;
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  try {
+    let res = await axios.get(`${apiURL}/api/category/all-category`, Headers());
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const createCategory = async ({
+  cName,
+  cImage,
+  cDescription,
+  cStatus,
+}) => {
+  let formData = new FormData();
+  formData.append("cImage", cImage);
+  formData.append("cName", cName);
+  formData.append("cDescription", cDescription);
+  formData.append("cStatus", cStatus);
+
+  try {
+    let res = await axios.post(
+      `${apiURL}/api/category/add-category`,
+      formData,
+      Headers()
+    );
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const editCategory = async (cId, des, status) => {
+  let data = { cId: cId, cDescription: des, cStatus: status };
+  try {
+    let res = await axios.post(
+      `${apiURL}/api/category/edit-category`,
+      data,
+      Headers()
+    );
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
