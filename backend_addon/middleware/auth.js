408165d80a287c2ad6a1930e1d423313f18d6dce
fineboy0407 <levilee.fine@gmail.com>
2023-04-07 08:24:06 -0400

    feat: finished auth script

--- backend_addon/middleware/auth.js
+++ backend_addon/middleware/auth.js
@@ -15,3 +15,28 @@ exports.loginCheck = (req, res, next) => {
     });
   }
 };
+
+exports.isAuth = (req, res, next) => {
+  let { loggedInUserId } = req.body;
+  if (
+    !loggedInUserId ||
+    !req.userDetails._id ||
+    loggedInUserId != req.userDetails._id
+  ) {
+    res.status(403).json({ error: "You are not authenticate" });
+  }
+  next();
+};
+
+exports.isAdmin = async (req, res, next) => {
+  try {
+    let reqUser = await userModel.findById(req.body.loggedInUserId);
+    // If user role 0 that's mean not admin it's customer
+    if (reqUser.userRole === 0) {
+      res.status(403).json({ error: "Access denied" });
+    }
+    next();
+  } catch {
+    res.status(404);
+  }
+};
