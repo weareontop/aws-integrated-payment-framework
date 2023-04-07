7839230e953fb43220a8bdaf9785e49ada82a270
bitcooker <dreamhighpassionhigh@gmail.com>
2023-04-07 07:51:56 -0400

    feat: config BearerToken

---
+++ frontend_addon/internal-troubleshooting-ui/src/components/admin/Categories/FetchApi.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+const apiURL = process.env.REACT_APP_API_URL;
+
+const BearerToken = () =>
+  localStorage.getItem("jwt")
+    ? JSON.parse(localStorage.getItem("jwt")).token
+    : false;
+const Headers = () => {
+  return {
+    headers: {
+      token: `Bearer ${BearerToken()}`,
+    },
+  };
+};
+
+
